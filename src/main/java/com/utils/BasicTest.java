519972643015250c045ab799b853636d672a4ad7
Khanh Pham <duykhanhrc@gmail.com>
2023-07-09 17:37:00 +0700

    refresh

---
+++ src/main/java/com/utils/BasicTest.java
@@ -0,0 +1,39 @@
+package com.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.concurrent.TimeUnit;
+
+
+public abstract class BasicTest {
+    
+    public static final Logger logger = LogManager.getLogger();
+    protected static WebDriver driver;
+    // private String driverPath;
+
+    @BeforeMethod
+    public void preCondition() {
+        // Chromedriver path
+        // driverPath = "src/main/resources/WebDrivers/chromedriver.exe";
+        // ChromeOptions options = new ChromeOptions();
+        // System.setProperty("webdriver.chrome.driver", driverPath);
+        // driver = new ChromeDriver(options);
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        // Maximize the browser
+        driver.manage().window().maximize();
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void postCondition(){
+        // Quit the Browser
+        driver.quit();
+    }
+}
---
+++ src/test/java/com/LoginTest.java
@@ -0,0 +1,19 @@
+package com;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.utils.BasicTest;
+
+public class LoginTest extends BasicTest {
+
+
+    @Test()
+    public void loginTest() throws Exception {
+        // Launch website
+        String url = "https://seniorautomationtest.com/";
+        driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+    }
+
+}
