edbbf40cf3e59f33e9916f7481e6da8ae9ef7160
Anna <anna.hotester1205@gmail.com>
2023-11-05 21:19:49 +0700

    Update homework-15

--- src/test/java/com/LoginTest.java
+++ src/test/java/com/LoginTest.java
@@ -7,11 +7,10 @@ import com.utils.BasicTest;
 
 public class LoginTest extends BasicTest {
 
-
     @Test()
     public void loginTest() throws Exception {
         // Launch website
-        String url = "https://seniorautomationtest.com/";
+        String url = "https://automationtestpro.com/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
     }
