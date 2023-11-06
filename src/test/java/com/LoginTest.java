cba3325e2b04066d9f0dbe71dc92054622a32fdc
hoangtri88888 <hoangtri88888@gmail.com>
2023-11-06 22:00:33 +0700

    nghĩa thay đổi

--- src/test/java/com/LoginTest.java
+++ src/test/java/com/LoginTest.java
@@ -11,6 +11,7 @@ public class LoginTest extends BasicTest {
     @Test()
     public void loginTest() throws Exception {
         // Launch website
+        // nghĩa cmt
         String url = "https://seniorautomationtest.com/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
