a94c8c1a272d7d16f28cbfdc4b2de2d178dd114b
Anna <anna.hotester1205@gmail.com>
2023-11-15 07:56:31 +0700

    update script bai17-18

--- src/test/java/com/LoginTest.java
+++ src/test/java/com/LoginTest.java
@@ -1,4 +1,5 @@
 package com;
+
 import java.util.ArrayList;
 
 import org.openqa.selenium.By;
@@ -25,104 +26,108 @@ public class LoginTest extends BasicTest {
         Utils.hardWait();
     }
 
-    public void verifyTextRegisterAndEmailInvalid(){
+    public void verifyTextRegisterAndEmailInvalid() {
         WebElement message = driver.findElement(By.xpath("//*[text()=' Vui lòng cung cấp địa chỉ email hợp lệ.\t\t']"));
         String actual = message.getText().trim();
         String expected = "Lỗi: Vui lòng cung cấp địa chỉ email hợp lệ.";
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
         Utils.hardWait();
     }
 
-    @Test(enabled=true)
-    public void loginTestBai16() {
-        // Launch website
+    public void InputText(By element, String text) {
+        WebElement emailEle1 = driver.findElement(element);
+        emailEle1.sendKeys(text);
+    }
+
+    public void Login() {
+        By userNameEle = By.xpath("//*[@name='username']");
+        String username = "huou@gmail.com";
+        InputText(userNameEle, username);
+        By passEle = By.xpath("//*[@name='password']");
+        String pass = "Huou123@123";
+        InputText(passEle, pass);
+        ClickLoginBtn();
+
+    }
+
+    public void Launchwebsite() {
         String url = "https://bantheme.xyz/hathanhauto/tai-khoan/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
+    }
 
+    @Test(enabled = true)
+    public void loginTestBai16() {
+        // Launch website
+        Launchwebsite();
         // Homework 16
         // Case register
         ClickRegisterBtn();
         verifyTextRegisterAndEmailInvalid();
 
         // Case emailInvalid
-        WebElement emailEle1 = driver.findElement(By.id("reg_email"));
-        emailEle1.sendKeys("123@456");
+        By emailEle1 = By.id("reg_email");
+        String email = "123@456";
+        InputText(emailEle1, email);
         ClickRegisterBtn();
         // Verify emailInvalid
         verifyTextRegisterAndEmailInvalid();
         Utils.hardWait();
-        
+
         // Case emailValid
         WebElement emailEle2 = driver.findElement(By.id("reg_email"));
         emailEle2.clear();
         emailEle2.sendKeys("qwerty@gmail.com");
         ClickRegisterBtn();
         // Verify emailValid
-        WebElement messageEmailValid = driver.findElement(By.xpath("//*[text()=' Vui lòng nhập mật khẩu tài khoản.\t\t']"));
+        WebElement messageEmailValid = driver
+                .findElement(By.xpath("//*[text()=' Vui lòng nhập mật khẩu tài khoản.\t\t']"));
         String actualEmailValid = messageEmailValid.getText().trim();
         String expectedEmailValid = "Lỗi: Vui lòng nhập mật khẩu tài khoản.";
-        Assert.assertEquals(expectedEmailValid,actualEmailValid);
+        Assert.assertEquals(expectedEmailValid, actualEmailValid);
 
     }
 
-    @Test(enabled=true)
+    @Test(enabled = true)
     public void loginTestBai17() {
-
         // Launch website
-        String url = "https://bantheme.xyz/hathanhauto/tai-khoan/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+        Launchwebsite();
 
         // Homework 17
-        //Case Login
-        WebElement userNameEle = driver.findElement(By.xpath("//*[@name='username']"));
-        userNameEle.sendKeys("huou@gmail.com");
-
-        WebElement passEle = driver.findElement(By.xpath("//*[@name='password']"));
-        passEle.sendKeys("Huou123@123");
-        ClickLoginBtn();
-
-        WebElement exitEle = driver.findElement(By.xpath("//*[text()='Thoát']"));
+        // Case Login
+        // WebElement userNameEle =
+        // driver.findElement(By.xpath("//*[@name='username']"));
+        // userNameEle.sendKeys("huou@gmail.com");
+        // WebElement passEle = driver.findElement(By.xpath("//*[@name='password']"));
+        // passEle.sendKeys("Huou123@123");
+        Login();
+        WebElement exitElement = driver.findElement(By.xpath("//*[text()='Thoát']"));
         Actions action = new Actions(driver);
-        action.moveToElement(exitEle).click(exitEle).build().perform();
+        action.moveToElement(exitElement).click(exitElement).build().perform();
         Utils.hardWait();
-        action.sendKeys(Keys.RETURN);
-        action.perform();
+        driver.navigate().back();
         Utils.hardWait();
-        WebElement textRegister = driver.findElement(By.xpath("//h2[text()='Đăng ký']"));
-        Assert.assertTrue(textRegister.isDisplayed());
+        WebElement verifyHeader = driver.findElement(By.xpath("//h1[contains(text(),'Tài khoản')]"));
+        Assert.assertTrue(verifyHeader.isDisplayed());
 
     }
 
-    @Test(enabled=true)
+    @Test(enabled = true)
     public void loginTestBai18() {
-
         // Launch website
-        String url = "https://bantheme.xyz/hathanhauto/tai-khoan/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+        Launchwebsite();
 
         // Homework 18
-        //Case Login
-        WebElement userNameEle = driver.findElement(By.xpath("//*[@name='username']"));
-        userNameEle.sendKeys("huou@gmail.com");
-
-        WebElement passEle = driver.findElement(By.xpath("//*[@name='password']"));
-        passEle.sendKeys("Huou123@123");
-        ClickLoginBtn();
-
-        //cach 1
-        driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL +"t");  
-        //Switch focus to new tab
-        ArrayList<String> tabs = new ArrayList<String> (driver.getWindowHandles()); 
+        // Case Login
+        Login();
+        // Way 1
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "t");
+        // Switch focus to new tab
+        ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
         driver.switchTo().window(tabs.get(0));
-        //Launch URL in the new tab
+        // Launch URL in the new tab
         driver.get("https://bantheme.xyz/hathanhauto/");
         Assert.assertEquals(driver.getCurrentUrl(), "https://bantheme.xyz/hathanhauto/");
 
     }
-    
-
-
 }
