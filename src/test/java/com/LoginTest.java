f6fddf7497860c5580e1a7b2cf52e4313ee6f7e0
Tam <tamtran0524@gmail.com>
2023-11-15 22:19:34 +0100

    change url

--- src/test/java/com/LoginTest.java
+++ src/test/java/com/LoginTest.java
@@ -11,7 +11,8 @@ public class LoginTest extends BasicTest {
     @Test()
     public void loginTest() throws Exception {
         // Launch website
-        String url = "https://seniorautomationtest.com/";
+        //String url = "https://seniorautomationtest.com/";
+        String url = "https://www.google.com/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
     }
