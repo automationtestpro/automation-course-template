974cd41c4c2882d9d95b13dad9f97f6f94e3b21f
Tam <tamtran0524@gmail.com>
2024-01-05 20:09:00 +0100

    Bai 19 + Bai 20

---
+++ src/test/java/com/Bai19.java
@@ -0,0 +1,53 @@
+package com;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import com.utils.BasicTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class Bai19 extends BasicTest {
+
+    /**
+         * @param id
+         * @param password
+         */
+    @Test(dataProvider = "testdata")
+    public void signupTest(String id, String password) {
+        // Launch website
+        String url = "https://icehrm-open.gamonoid.com/login.php";
+        driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='username']"))).sendKeys(id);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='password']"))).sendKeys(password);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='loginForm']//button"))).click();
+        try {
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//i[@class='fa fa-circle text-success']")));
+
+        } catch (NoSuchElementException e) {
+            System.out.println("The 'Logged in' status could not be found.");
+        }
+        driver.quit();
+    }
+
+    @DataProvider(name="testdata")
+    public Object[][] logindata(){
+        return new Object[][]
+        {
+            {"admin","admin"},
+            {"manager","demouserpwd"},
+            {"user1","demouserpwd"},
+            {"user2","demouserpwd"}
+        };
+    }
+
+}
