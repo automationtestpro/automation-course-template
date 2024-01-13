ce6c00b434b642f11f84634f9662d841dc1bde55
nhatthuy <nhatthuyld@gmail.com>
2024-01-13 10:01:09 +0700

    Update LoginTest.java
    all step

--- src/test/java/com/LoginTest.java
+++ src/test/java/com/LoginTest.java
@@ -1,5 +1,7 @@
 package com;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,7 +16,33 @@ public class LoginTest extends BasicTest {
         String url = "https://bantheme.xyz/hathanhauto/tai-khoan/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
-        System.out.println("abc");
+        
+        //Step1: Click  submit button
+        WebElement submitBtn = driver.findElement(By.xpath("//button[contains(text(),'Đăng ký')]"));
+        submitBtn.click();
+        WebElement errorMessage = driver.findElement(By.xpath("//div[@id='main']//li[1]"));
+        System.out.println(errorMessage.getText());
+        Assert.assertEquals(errorMessage.getText(),"Lỗi: Vui lòng cung cấp địa chỉ email hợp lệ.");
+
+        //Step2: Email invalid
+        WebElement email = driver.findElement(By.xpath("//input[@id='reg_email']"));
+        email.sendKeys("123@456");
+        WebElement submitBtn2 = driver.findElement(By.xpath("//button[contains(text(),'Đăng ký')]"));
+        submitBtn2.click();
+        WebElement errorMessage2 = driver.findElement(By.xpath("//div[@id='main']//li[1]"));
+        Assert.assertEquals(errorMessage2.getText(),"Lỗi: Vui lòng cung cấp địa chỉ email hợp lệ.");
+
+
+        //Step3:Valid email, missing password
+        WebElement email3 = driver.findElement(By.xpath("//input[@id='reg_email']"));
+        email3.clear();
+        email3.sendKeys("thuytran@gmail.com");
+        WebElement submitBtn3 = driver.findElement(By.xpath("//button[contains(text(),'Đăng ký')]"));
+        submitBtn3.click();
+        WebElement errorMessage3 = driver.findElement(By.xpath("//div[@id='main']//li[1]"));
+        Assert.assertEquals(errorMessage3.getText(),"Lỗi: Vui lòng nhập mật khẩu tài khoản.");
+
+
     }
 
 }
