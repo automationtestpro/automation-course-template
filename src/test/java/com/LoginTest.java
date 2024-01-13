cffb9b07bc392d40f4cc3b1b5046f2580dac8b8d
nhatthuy <nhatthuyld@gmail.com>
2024-01-13 19:47:28 +0700

    Update LoginTest.java

--- src/test/java/com/LoginTest.java
+++ src/test/java/com/LoginTest.java
@@ -14,6 +14,7 @@ public class LoginTest extends BasicTest {
         String url = "https://automationtestpro.com/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
+        System.out.println("abc");
     }
 
 }
