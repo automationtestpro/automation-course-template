1c3063027a2dbe36df23208238c1451d2d70e465
nhatthuy <nhatthuyld@gmail.com>
2024-01-14 06:38:46 +0700

    bai17.java
    done all

--- src/test/java/com/LoginTest.java
+++ src/test/java/com/LoginTest.java
@@ -1,5 +1,7 @@
 package com;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,11 +12,29 @@ public class LoginTest extends BasicTest {
 
     @Test()
     public void loginTest() throws Exception {
-        // Launch website
-        String url = "https://automationtestpro.com/";
+        // Log in
+        String url = "https://bantheme.xyz/hathanhauto/tai-khoan/";
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        System.out.println("abc");
+        WebElement email = driver.findElement(By.xpath("//input[@id='username']"));
+        email.sendKeys("nhatthuyip@gmail.com");
+        WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
+        password.sendKeys("Bgr2ftvvYsY8hVe");
+        WebElement btnLogin = driver.findElement(By.xpath("//button[contains(text(),'Đăng nhập')]"));
+        btnLogin.click();
+
+        // Log out
+        WebElement thoat = driver.findElement(By.xpath("//a[normalize-space()='Thoát']"));
+        thoat.click();
+
+        // Back page
+        driver.navigate().back();
+        
+        // Verirfy can not visit account page 
+        WebElement loginbtn = driver.findElement(By.xpath("//button[contains(text(),'Đăng nhập')]"));
+        Assert.assertTrue(loginbtn.isDisplayed());
+
+
+
     }
 
 }
