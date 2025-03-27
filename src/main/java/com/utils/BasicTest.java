b88e43fdba5a7a2a544b31e85de90bd4277638fb
Khanh Pham <duykhanhrc@gmail.com>
2025-03-27 21:43:17 +0700

    update BasicTest.java

--- src/main/java/com/utils/BasicTest.java
+++ src/main/java/com/utils/BasicTest.java
@@ -28,8 +28,14 @@ public abstract class BasicTest {
         // ChromeOptions options = new ChromeOptions();
         // System.setProperty("webdriver.chrome.driver", driverPath);
         // driver = new ChromeDriver(options);
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("window-size=1920,1080");
+        options.addArguments("--no-sanbox");
+
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         // Maximize the browser
         driver.manage().window().maximize();
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
