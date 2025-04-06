2f8c944d50009dc8f966caadd2bf90d0ea4c5e0c
Khanh Pham <duykhanhrc@gmail.com>
2025-04-06 16:02:03 +0700

    a

--- src/test/java/com/LoginTest.java
+++ src/test/java/com/LoginTest.java
@@ -1,9 +1,11 @@
 package com;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.utils.BasicTest;
+import com.utils.Utils;
 
 public class LoginTest extends BasicTest {
 
@@ -11,9 +13,27 @@ public class LoginTest extends BasicTest {
     @Test()
     public void loginTest() throws Exception {
         // Launch website
-        String url = "https://automationtestpro.com/";
+        String url = "https://the-internet.herokuapp.com/login";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
+        Utils.hardWait();
+
+        // Enter username
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        Utils.hardWait();
+
+        // Enter password
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        Utils.hardWait();
+
+        // Click on login button
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Utils.hardWait();
+
+        // Verify login success
+        Assert.assertTrue(driver.findElement(By.xpath("//h4[contains(text(),'Welcome to the Secure Area.')]")).isDisplayed(), "Login failed");
+        Utils.hardWait();
+
     }
 
 }
