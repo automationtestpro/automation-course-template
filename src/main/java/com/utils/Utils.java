8e9c4f43c756a06fa4c5a93224f9dc0cb0d876ce
Minh Dang <mdangdn29@gmail.com>
2025-11-17 07:41:54 +0700

    up

--- src/main/java/com/utils/Utils.java
+++ src/main/java/com/utils/Utils.java
@@ -1,5 +1,13 @@
 package com.utils;
 
+
+import java.io.File; // 
+import java.io.IOException;
+import org.apache.commons.io.FileUtils; // 
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.OutputType;
+
 public class Utils {
     public static void hardWait(int timeout){
         try {
@@ -11,6 +19,12 @@ public class Utils {
 
     public static void hardWait(){
         hardWait(3000);
+    }
+    public static void takeScreenshot(WebDriver driver, String name) throws IOException {
+        // Implementation for taking screenshot
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        // Now you can do whatever you need to do with it, for example copy somewhere
+        FileUtils.copyFile(scrFile, new File("target/screenshots/" + name + ".png"));
     }
 
 }
--- src/test/java/com/Bai26_OrderProductTest.java
+++ src/test/java/com/Bai26_OrderProductTest.java
@@ -27,10 +27,11 @@ public class Bai26_OrderProductTest extends BasicTest {
         // Launch website
         homepage.open(homepage.url);
         Utils.hardWait(1000);
-
+        Utils.takeScreenshot(driver, "img1");
        // check banner
         boolean isBannerDisplayed = isElementDisplayedQuickly( By.id("VIP_BUNDLE"), 5);        
-         
+        Utils.takeScreenshot(driver, "img2");
+
         if (isBannerDisplayed) {
             System.out.println("Banner found, closing it...");
             By closeButtonLocator = By.xpath("//img[@alt='close-icon']");
@@ -42,12 +43,16 @@ public class Bai26_OrderProductTest extends BasicTest {
         
         // click item menu
         homepage.clickCategory();
-        
+        Utils.takeScreenshot(driver, "img3");
+
         // click Item đầu tiên
-         collectpage.clickMenu();
+        collectpage.clickMenu();
+        Utils.takeScreenshot(driver, "img4");
+
         Utils.hardWait(2000);
 
          collectpage.clickItem();
+        Utils.takeScreenshot(driver, "img5");
         Utils.hardWait(3000);
 
         // // add Item cart
@@ -68,7 +73,11 @@ public class Bai26_OrderProductTest extends BasicTest {
         // cartpage.clickPlus();
         // Utils.hardWait(500);
         // plus Item ->False sticky
+        // take screen shot
         cartpage.addItemCart();
+        Utils.takeScreenshot(driver, "img6");
+        Utils.hardWait(2000);
+        
         String unitPriceStr = cartpage.getUnitPrice();
         Utils.hardWait(2000);
         String totalPriceStr = cartpage.getTotalPrice();
